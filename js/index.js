4d5955608739793b660b755ebf41c7946bc9f173
Neill Bogie <neillbogie@googlemail.com>
2016-06-23 04:43:02 +0100

    added an angular material md-select for the list of games. feels heavyweight.  many deps.

--- js/index.js
+++ js/index.js
@@ -5,20 +5,21 @@
     /*global $ */
     /*global console */
 
-    var app = angular.module('twitchApp', []);
+    var app = angular.module('twitchApp', ['ngMaterial']);
 
     app.controller('StreamController', function () {
 
         var channelNameList = ['esl_sc2', 'freecodecamp', 'comster404', 'habathcx', 'ogamingsc2'];
 
-        this.game = "";
+        this.game = "League of Legends";
+        this.possibleGames = ["one", "two", "three"];
         this.streams = [];
+
         var that = this;
 
-        //$('#gameSelect option:selected');
-        var getForGame = function () {
-            var game = that.game;
-            if (game !== '-') {
+        this.getForGame = function () {
+            console.log('get for game, and ' + that.game );
+            if (that.game !== '-') {
                 $.getJSON('https://api.twitch.tv/kraken/streams/?', {
                         'game': that.game,
                         'limit': 8
@@ -37,10 +38,23 @@
                 function (data) {
                     //get others: (data, channelNameList);
                     that.streams = data;
-                    console.log('retrieved ' + that.streams.length + ' streams from json');
+                    //console.log('retrieved ' + that.streams.length + ' streams from json');
                 }
             );
         };
-        getForGame();
-    });
-}());
+
+        var getGames = function () {
+            //$.getJSON('https://api.twitch.tv/kraken/games/top?limit=30', function (data) {
+            $.getJSON('sample-json/games.json', function (data) {
+                var names = data.top.map(function (g) {
+                    return g.game.name;
+                });
+                console.log("game names: "+ names);
+                that.possibleGames = names;
+            });
+        };
+
+        getGames();
+//        getForGame();
+    }); // ends controller
+}()); //ends the wrapping IIFE
