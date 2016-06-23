9616c238534645d4e613ca6a217cef7dffded5cb
Neill Bogie <neillbogie@googlemail.com>
2016-06-23 03:26:44 +0100

    loads streams and displays them using a template stream.html.  50% done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,46 @@
-/*global angular */
-var app = angular.module('twitchApp', []);
+(function () {
 
-app.controller('StreamController', function(){
-    this.game = "";
-    this.foos = [10,20,30,100,1000,50000];
-    this.streams = [];
-});
+    'use strict';
+    /*global angular */
+    /*global $ */
+    /*global console */
+
+    var app = angular.module('twitchApp', []);
+
+    app.controller('StreamController', function () {
+
+        var channelNameList = ['esl_sc2', 'freecodecamp', 'comster404', 'habathcx', 'ogamingsc2'];
+
+        this.game = "";
+        this.streams = [];
+        var that = this;
+
+        //$('#gameSelect option:selected');
+        var getForGame = function () {
+            var game = that.game;
+            if (game !== '-') {
+                $.getJSON('https://api.twitch.tv/kraken/streams/?', {
+                        'game': that.game,
+                        'limit': 8
+                    },
+                    function (data) {
+                        console.log(JSON.stringify(data, null, 2));
+                        that.streams = data.streams;
+                    });
+            }
+        };
+
+        var getFaves = function () {
+            $.getJSON('https://api.twitch.tv/kraken/streams/?', {
+                    'channel': channelNameList.join(',')
+                },
+                function (data) {
+                    //get others: (data, channelNameList);
+                    that.streams = data;
+                    console.log('retrieved ' + that.streams.length + ' streams from json');
+                }
+            );
+        };
+        getForGame();
+    });
+}());
